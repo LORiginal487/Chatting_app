0a294681a81ccb8685811f828ad0501ae99747c4
LORiginal487 <oscarravuku20@gmail.com>
2023-10-27 12:20:16 +0200

    1.0.2.0
    we moving into the chat screen

---
+++ app/src/main/java/com/example/chatting_app/ChatActivity.java
@@ -0,0 +1,21 @@
+package com.example.chatting_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+    public void Go_Back(View view) {
+    }
+
+    public void pressSend(View view) {
+    }
+}
